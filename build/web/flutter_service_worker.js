edf1374e9aff706db3f37c62db32ccfa1434f22c
Sejal-08 <sejalsankhyan2001@gmail.com>
2026-01-30 10:20:51 +0530

    Add Flutter web build files

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -21,7 +21,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "0b0a3415aad49b6e9bf965ff578614f9
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "b069af72db6f2a398b2fdd1297bdb430",
+"flutter_bootstrap.js": "aec564731c53c0041264f3de5cf7cef4",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
